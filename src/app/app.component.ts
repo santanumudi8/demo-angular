2c190931b87a530b8ad96572467c6cebd712ec07
Santanu Mudi <santanumudi8@gmail.com>
2023-12-23 15:06:47 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo';
+  song = new Audio();
+  vSong = new RTCEncodedVideoFrame();
+  songs=[
+    {
+      url:'./assets/Song1.mp3',
+      name:'Song Name'
+    },
+    {
+      url:'./assets/Song1.mp3',
+      name:'Song Name 2'
+    }
+  ]
+
+  openSong(song:any){
+    this.song.src = song;
+    this.song.load();
+    this.song.play();
+  }
+
+  playSong(){
+    this.song.play();
+  }
+  pauseSong(){
+    this.song.pause();
+  }
+  stopSong(){
+    this.song.pause();
+    this.song.currentTime = 0;
+  }
+  vol(vl:any){
+    this.song.volume=vl.target.value;
+  }
+  progress(prog:any){
+    this.song.currentTime=prog.target.value;
+  }
+
 }
